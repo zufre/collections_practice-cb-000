a59d6a1bdd05eaa9439369706ac0bbe761ae5b19
Cedric David <cedric.david@btopenworld.com>
2018-09-02 14:50:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,74 +1,48 @@
-require 'pry'
+def sort_array_asc(arr)
+  arr.sort
+end
 
-def sort_array_asc(array)
-  array.sort do |a,b|
-    a <=> b
+def sort_array_desc(arr)
+  arr.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    a.length <=> b.length
   end
 end
 
-def sort_array_desc(array)
-  array.sort do | left, right|
-    right <=> left
-  end
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
 end
 
-def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
-  end
+def reverse_array(arr)
+  arr.reverse
 end
 
-def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array
-end
-
-def reverse_array(array)
-  array.reverse
-end
-
-def kesha_maker(array)
-  array.each do |item|
+def kesha_maker(arr)
+  arr.each do |item|
     item[2] = "$"
   end
 end
 
-def find_a(array)
-  array.find_all do |word|
-    word[0] == "a"
+def find_a(arr)
+  arr.select do |word|
+    word.start_with?("a")
   end
-
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
 end
 
-def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum+=num
-  end
-  sum
-
-  # using reduce method
-    # array.reduce(:+)
-
-  # using inject method (short)
-     # array.inject(:+)
-
-  # using inject method (long)
-     # array.inject do |sum,x|
-     #  sum + x
-     # end
+def sum_array(arr)
+  arr.inject {|sum, n| sum + n}
 end
 
-def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
-    else
-      word + "s"
-    end
+def add_s(arr)
+  arr.each_with_index.collect {|el, i|
+  if  i == 1
+    el
+  else
+    "#{el}s"
   end
+  }
 end
